db9e7ce29bc87b6b50a8d39770302e0f78c92552
Ben <ben.atkinson.official@gmail.com>
2022-12-06 20:44:27 +1100

    day 6 unthreaded

--- src/day_06.rs
+++ src/day_06.rs
@@ -1,22 +1,17 @@
-use core::time;
-use std::{collections::HashSet, fs, str::from_utf8, thread};
+use std::{collections::HashSet, fs, str::from_utf8};
 
-use termion::{clear, color, cursor::Goto};
+use termion::color;
 
 pub fn part_01() -> usize {
     let s = fs::read_to_string("./src/input_06.txt").expect("couldn't read input file");
     let input = s.lines().take(1).next().expect("oh noes").as_bytes();
     let back_check = 4;
     let mut set: HashSet<u8>;
-    let timeout = time::Duration::from_millis(10);
-    print!("{}", clear::All);
-
     for i in back_check..input.len() {
         set = (i - back_check..i).map(|n| input[n]).collect();
         if set.len() == back_check {
             print!(
-                "{}{}{}",
-                Goto(1, 1),
+                "{}{}",
                 color::Fg(color::Green),
                 from_utf8(&input[i - back_check..i]).unwrap()
             );
@@ -28,21 +23,7 @@ pub fn part_01() -> usize {
             println!("{}{}", color::Fg(color::Green), i);
             return i;
         }
-        print!(
-            "{}{}{}",
-            Goto(1, 1),
-            color::Fg(color::Red),
-            from_utf8(&input[i - back_check..i]).unwrap()
-        );
-        println!(
-            "{}{}{}",
-            Goto((back_check + 1).try_into().unwrap(), 1),
-            color::Fg(color::Blue),
-            from_utf8(&input[i..i + 50]).unwrap()
-        );
         set.clear();
-
-        thread::sleep(timeout);
     }
     return 0;
 }
@@ -52,13 +33,11 @@ pub fn part_02() -> usize {
     let input = s.lines().take(1).next().expect("oh noes").as_bytes();
     let back_check = 14;
     let mut set: HashSet<u8>;
-    let timeout = time::Duration::from_millis(10);
     for i in back_check..input.len() {
         set = (i - back_check..i).map(|n| input[n]).collect();
         if set.len() == back_check {
             print!(
-                "{}{}{}",
-                Goto(1, 4),
+                "{}{}",
                 color::Fg(color::Green),
                 from_utf8(&input[i - back_check..i]).unwrap()
             );
@@ -70,20 +49,7 @@ pub fn part_02() -> usize {
             println!("{}{}", color::Fg(color::Green), i);
             return i;
         }
-        print!(
-            "{}{}{}",
-            Goto(1, 4),
-            color::Fg(color::Red),
-            from_utf8(&input[i - back_check..i]).unwrap()
-        );
-        println!(
-            "{}{}{}",
-            Goto((back_check + 1).try_into().unwrap(), 4),
-            color::Fg(color::Blue),
-            from_utf8(&input[i..i + 40]).unwrap()
-        );
         set.clear();
-        thread::sleep(timeout);
     }
     return 0;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
-use std::thread;
-
 pub mod day_01;
 pub mod day_06;
 fn main() {
     day_01::part_01();
     day_01::part_02();
-    let handle = thread::spawn(|| {
-        day_06::part_01();
-    });
+    day_06::part_01();
     day_06::part_02();
-
-    handle.join().unwrap();
 }
